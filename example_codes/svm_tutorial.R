dd41070f059c233d6172a0090e22e9b5c453ebe1
lewisKit <lewisfyh2012@gmail.com>
2020-10-20 14:26:23 -0500

    Create svm_tutorial.R

---
+++ example_codes/svm_tutorial.R
@@ -0,0 +1,86 @@
+data(iris)
+attach(iris)
+
+## classification mode
+# default with factor response:
+model <- svm(Species ~ ., data = iris)
+
+# alternatively the traditional interface:
+x <- subset(iris, select = -Species)
+y <- Species
+model <- svm(x, y) 
+
+print(model)
+summary(model)
+
+# test with train data
+pred <- predict(model, x)
+# (same as:)
+pred <- fitted(model)
+
+# Check accuracy:
+table(pred, y)
+
+# compute decision values and probabilities:
+pred <- predict(model, x, decision.values = TRUE)
+attr(pred, "decision.values")[1:4,]
+
+# visualize (classes by color, SV by crosses):
+plot(cmdscale(dist(iris[,-5])),
+     col = as.integer(iris[,5]),
+     pch = c("o","+")[1:150 %in% model$index + 1])
+
+## try regression mode on two dimensions
+
+# create data
+x <- seq(0.1, 5, by = 0.05)
+y <- log(x) + rnorm(x, sd = 0.2)
+
+# estimate model and predict input values
+m   <- svm(x, y)
+new <- predict(m, x)
+
+# visualize
+plot(x, y)
+points(x, log(x), col = 2)
+points(x, new, col = 4)
+
+
+
+# 
+## weights: (example not particularly sensible)
+i2 <- iris
+levels(i2$Species)[3] <- "versicolor"
+summary(i2$Species)
+wts <- 100 / table(i2$Species)
+wts
+m <- svm(Species ~ ., data = i2, class.weights = wts)
+
+## extract coefficients for linear kernel
+
+# a. regression
+x <- 1:100
+y <- x + rnorm(100)
+m <- svm(y ~ x, scale = FALSE, kernel = "linear")
+coef(m)
+plot(y ~ x)
+abline(m, col = "red")
+
+# b. classification
+# transform iris data to binary problem, and scale data
+setosa <- as.factor(iris$Species == "setosa")
+iris2 = scale(iris[,-5])
+
+# fit binary C-classification model
+m <- svm(setosa ~ Petal.Width + Petal.Length,
+         data = iris2, kernel = "linear")
+
+# plot data and separating hyperplane
+plot(Petal.Length ~ Petal.Width, data = iris2, col = setosa)
+(cf <- coef(m))
+abline(-cf[1]/cf[3], -cf[2]/cf[3], col = "red")
+
+# plot margin and mark support vectors
+abline(-(cf[1] + 1)/cf[3], -cf[2]/cf[3], col = "blue")
+abline(-(cf[1] - 1)/cf[3], -cf[2]/cf[3], col = "blue")
+points(m$SV, pch = 5, cex = 2)
